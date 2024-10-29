089f27f0be8216a90cdee6f70fff091abd47ea50
강세민 <semin15151@gmail.com>
2024-10-30 06:08:06 +0900

    [level 1] Title: 콜라 문제, Time: 0.08 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int c = 0;
+        while(n>=a)
+        {
+            c+=(n/a)*b;
+            n=((n/a)*b)+(n%a);  
+        }
+        return c;
+    }
+}
