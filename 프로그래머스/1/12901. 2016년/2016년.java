c61e2125565e62e7e11b89160cfcbfc617767837
강세민 <semin15151@gmail.com>
2024-10-30 19:55:14 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 70.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int a, int b) {
+        String[] c = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int[] d = {31,29,31,30,31,30,31,31,30,31,30,31};
+        int e=0;
+        for(int i=0;i<a-1;i++)
+            e+=d[i];
+        e= (e+b-1)%7;     
+        return c[(e)];
+    }
+}
