b60a5f460c2ce628e2b1e684b04986d58cb32191
강세민 <semin15151@gmail.com>
2024-10-31 03:49:33 +0900

    [level 1] Title: 소수 만들기, Time: 19.31 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] n) {
+        int a =0;
+        List<Integer> b= new ArrayList<Integer>();
+
+        for(int i=0;i<n.length-2;i++)
+        {for(int j=i+1;j<n.length-1;j++)
+          {for(int k=j+1;k<n.length;k++)
+            {
+              if(IsPrime(n[i]+n[j]+n[k],b))
+                  a++;
+            }
+          }
+        }
+        return a;
+    }
+    public boolean IsPrime(int a, List<Integer> b)
+    {
+        if(b.contains(a)) return true;
+        
+        for(int i=2;i<a;i++)
+        {
+            if(a%i==0) return false;
+        }
+        b.add(a);
+        return true;
+    }
+}
