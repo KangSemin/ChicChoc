20f39366297f857aef2bee1eb5c0734d8a56b11b
강세민 <semin15151@gmail.com>
2024-11-02 15:27:02 +0900

    [level 1] Title: 옹알이 (2), Time: 22.53 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -3,32 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(String[] b) {
         int d = 0;
-
-        HashMap<String, Integer> a = new HashMap<>();
-        a.put("aya", 0);
-        a.put("ye", 0);
-        a.put("woo", 0);
-        a.put("ma", 0);
-
+        List<String> a = new ArrayList<>();
+        a.add("aya");
+        a.add("ye");
+        a.add("woo");
+        a.add("ma");
         for (int i = 0; i < b.length; i++){
             String[] c = {"",""};
-
             for (int j = 0; j < b[i].length(); j++){
                 c[0] += b[i].charAt(j);
-
-                if ( !c[1].equals(c[0]) && a.containsKey(c[0]))
+                if ( !c[1].equals(c[0]) && a.contains(c[0]))
                 {
                     c[1] = c[0];
                     c[0] = "";
                 }
             }
-
             if (c[0].isEmpty()) d ++;
         }
-
-
-
-        return d;
-
+       return d;
     }
 }
