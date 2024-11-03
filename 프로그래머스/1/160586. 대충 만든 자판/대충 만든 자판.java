dc79b65eb412954af883d4e2e586ea6371af81b3
강세민 <semin15151@gmail.com>
2024-11-03 20:34:25 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.73 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -1,32 +1,30 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String[] b, String[] e) {
-        
-        HashMap<Character,Integer> a = new HashMap<>();
-        int[] d = new int[e.length];
+        int[] a = new int[26];
+        int[] d= new int[e.length];
         
         for( int i=0;i<b.length;i++)
         {
             int cnt=1;
             for(char c : b[i].toCharArray())
             {
-                if(!a.containsKey(c)) a.put(c,cnt);
-                else if(a.get(c)>cnt) a.replace(c,cnt);
+                if(a[c-'A']>cnt || a[c-'A']==0) a[c-'A']=cnt;
                 cnt++;
             }
         }
+        
         for(int i=0;i<e.length;i++)
-            
+        {
             for(char c:e[i].toCharArray())
             {
-                if(a.containsKey(c)) d[i]+= a.get(c);
+                if(a[c-'A']!=0)d[i]+= a[c-'A'];
                 else
                 {
                     d[i]=-1;
-                    break ;
+                    break;
                 }
-            
+                
+            }
         }
         
         return d;
